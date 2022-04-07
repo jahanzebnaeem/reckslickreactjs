fda55f046f51863e91650e7bd7cade562f87d486
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-04-07 13:33:41 +0500

    Lecture 1
    Introduction to javascript

---
+++ javascript/index.js
@@ -0,0 +1,30 @@
+x = 5;
+y = "5";
+
+alert(x != y);
+
+
+// var a = 5;
+// var a = "Jahanzeb";
+// var a = true;
+// alert(typeof(a));
+
+
+// alert(1+1);
+// alert(2-1);
+// alert(2*2);
+// alert(2**5);
+// alert(9/6);
+// alert(9%6)
+
+// var x = 5;
+// // x = x + 1;
+// // x++;
+// // x = x + 2;
+// x += 2;
+// alert(x);
+
+// var myName = "Jahanzeb Naeem";
+// var yourName = prompt("Your name please");
+
+// alert("I am learning the course of ReactJs from " + myName + " and the student name is " + yourName);
